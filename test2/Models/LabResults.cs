b38d4510a4ecf4b3244a3cb786f390050825beac
insatiable1906 <93722747+insatiable1906@users.noreply.github.com>
2021-11-04 12:27:14 -0400

    Add files via upload

---
+++ test2/Models/LabResults.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace test2.Models
+{
+    
+    public class LabResults
+    {
+        [Required(ErrorMessage = "LabType is required")]
+        public string LabType { get; set; }
+        public string Result { get; set; }
+        public string TestTime { get; set; }
+        [Required(ErrorMessage = "EnteredTime is required")]
+        public string EnteredTime { get; set; }
+        [Required(ErrorMessage = "PatientID is required")]
+        public Guid PatientID { get; set; }
+        [Key]
+        public Guid LabID { get; set; }
+    }
+}
